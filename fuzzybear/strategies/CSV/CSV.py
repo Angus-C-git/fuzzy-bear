ac8f44dc5ac4fc2b359a42d7c144dc52b8680fa4
zephyr <zephyr@zephyr>
2021-07-08 16:11:23 +1000

    ADDED :: Initial CSV genrator build (#1)

--- fuzzybear/strategies/CSV/CSV.py
+++ fuzzybear/strategies/CSV/CSV.py
@@ -5,11 +5,19 @@ CSV strategy driver Class
       inputs
 '''
 from .. import Strategy
-import csv
+from  csv import reader
+from random import randint
 
 
+# random row & column number
+# within the size of csv
+def random_row_col(range):
+    a, b = range
+    return (randint(0, b), randint(0, b))
 
-# random row number 10 - 1000
+
+def pack_csv(data):
+   return "\n".join((",".join(row_col) for row_col in data))
 
 
 
@@ -18,10 +26,37 @@ class CSV(Strategy.Strategy):
     # parse csv input data
     def __init__(self, sample_input):
         super()
-        self.raw_data = csv.reader(sample_input)
-        print(self.raw_data)
+        self.sample_input = sample_input
+        self.parse_csv()
 
-    
 
-    
+    def parse_csv(self):
+        with open(self.sample_input) as csvfile:
+            self.candidate_input = list(reader(csvfile))
 
+        self.size = (len(self.candidate_input) - 1, 
+                    len(self.candidate_input[0]) - 1)
+
+
+    # run strategies
+    def run(self):
+        print(f"\n[DEBUG] mutating {self.candidate_input} \n")        
+
+        row, col = random_row_col(self.size)
+        mutation = self.candidate_input
+        for emoji in super().emoji():
+            mutation[row][col] = emoji   
+            # print(mutation) 
+            yield pack_csv(mutation)
+        
+            
+
+
+
+'''dev_notes
+
+ + Even tho it matters for some CSVs that the header 
+ remains intact we can ignore this as it will eventually
+ be resolved by the generator 
+
+'''
