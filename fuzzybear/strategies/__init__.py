d6fb4f096c41b2b0ed7ede4a9fcec8ca142341c0
zephyr <zephyr@zephyr>
2021-07-08 21:38:52 +1000

    UPDATE :: imports

--- fuzzybear/strategies/__init__.py
+++ fuzzybear/strategies/__init__.py
@@ -3,7 +3,7 @@
 from .CSV import CSV
 from .ELF import ELF
 from .JPEG import JPEG
-# from .JSON import JSON
+from .JSON import JSON
 from .PDF import PDF
 from .TXT import TXT
 from .XML import XML
