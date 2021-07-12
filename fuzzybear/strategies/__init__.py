895baf14cc301127a5d3e5fc95c1bfc8c55ab0ba
zephyr <zephyr@zephyr>
2021-07-12 14:05:33 +1000

    UPDATED :: generators list, broken for unimplemented codecs

--- fuzzybear/strategies/__init__.py
+++ fuzzybear/strategies/__init__.py
@@ -12,7 +12,14 @@ from .XML import XML
 def get_generator(codec, sample_input):
 
     _generators = {
-        'csv': CSV.CSV(sample_input)
+        'csv': CSV.CSV(sample_input),
+        'elf': ELF.ELF(sample_input),
+        'jpeg': JPEG.JPEG(sample_input),
+        'json': JSON.JSON(sample_input),
+        'pdf': PDF.PDF(sample_input),
+        'plain': TXT.TXT(sample_input),
+        'octet-stream': TXT.TXT(sample_input),   # detects plaintext3 as octet-stream
+        'html': XML.XML(sample_input)           # detects xml as html
     }
 
     return _generators[codec]
