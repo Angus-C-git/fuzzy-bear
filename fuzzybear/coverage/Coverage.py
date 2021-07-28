aa9a08b7c968ea30751a247cdad124f9e06f65ad
Angus-C-git <anguscornall@gmail.com>
2021-07-28 21:15:58 +1000

    ADDED :: Example of other things that can be ripped from core

--- fuzzybear/coverage/Coverage.py
+++ fuzzybear/coverage/Coverage.py
@@ -23,18 +23,21 @@ def dump_symbols():
     # process.corefile(test_binary)
     proc = process(test_binary)
     #gdb.corefile(proc)
-
+    prog_state = proc.corefile
+    print(f"EIP @: {hex(prog_state.eip)}")
     proc.recvuntil('function1\n')
     proc.sendline('test')
-    proc.sendline('0')    
-    proc.recvuntil('function2\n')
+    # proc.sendline('0')    
+    # proc.recvuntil('function2\n')
 
-    prog_state = proc.corefile
     # dump a corefile at the current point
     # core is written to disk @ cwd so will
     # need cleanup
-    
+    prog_state = proc.corefile
     print(f"EIP @: {hex(prog_state.eip)}")
+
+    print(f"STACK :: {prog_state.stack}")
+    print(f"Registers :: {prog_state.registers}")
 
     # pid = gdb.attach(
     #     proc, 
@@ -64,5 +67,6 @@ dump_symbols()
 + pwntools corefile function is a wrapper around gdbs corefile method and
   can be called inline while the binary runs
 + There is overhead associated with this method of course
++ The method can be quite unreliable
 
 """
