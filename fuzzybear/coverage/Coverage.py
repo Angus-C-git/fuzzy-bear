c364a2528932511f5697d709732b63f2b2fa32fc
Angus-C-git <anguscornall@gmail.com>
2021-07-28 21:06:02 +1000

    ADDED :: basic attempts & planning for coverage

---
+++ fuzzybear/coverage/Coverage.py
@@ -0,0 +1,68 @@
+from pwn import *
+from pwnlib.gdb import corefile
+
+# import gdb
+# proc = process('../../tests/components/coverage/complex')
+
+test_binary = "../../tests/components/coverage/complex"
+
+_default_symbols = [
+    '_start',
+    '_init',
+    'stack_chk_fail'
+
+]
+
+def dump_symbols():
+    
+    elf = ELF(test_binary)
+    print(elf.symbols, "\n\n")
+    print("Functions: \n", elf.functions.keys(), "\n\n")
+    
+    
+    # process.corefile(test_binary)
+    proc = process(test_binary)
+    #gdb.corefile(proc)
+
+    proc.recvuntil('function1\n')
+    proc.sendline('test')
+    proc.sendline('0')    
+    proc.recvuntil('function2\n')
+
+    prog_state = proc.corefile
+    # dump a corefile at the current point
+    # core is written to disk @ cwd so will
+    # need cleanup
+    
+    print(f"EIP @: {hex(prog_state.eip)}")
+
+    # pid = gdb.attach(
+    #     proc, 
+    #     gdbscript='''
+    #             disassemble main 
+                 
+    #             quit
+    #             ''',
+    #     api=True
+    # )
+    # print(pid)
+
+    # print(proc.recvline())
+    # gdb_instance = gdb.
+
+    # gdb_pipe = gdb.debug([test_binary], gdbscript="""break main""", api=True)
+    
+    
+
+dump_symbols()
+
+
+"""devnotes
+
++ We can dump corefiles for the running binary to detect the state
+  of registers namely EIP which is critical to getting coverage information
++ pwntools corefile function is a wrapper around gdbs corefile method and
+  can be called inline while the binary runs
++ There is overhead associated with this method of course
+
+"""
