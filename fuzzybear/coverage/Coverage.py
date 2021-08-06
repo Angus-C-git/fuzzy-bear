de64e97db463116fd431159274d9dc2938e2767e
lucas <lucaskottas14@gmail.com>
2021-08-07 01:26:14 +1000

    Able to parse out basically all functions from the binary, storing both the instructions and function address in a data structure. Also able to parse out all code blocks that end in a jump and store the range of each block in a data structure. Code blocks that don't end in a jmp instruction are not captured properly.

--- fuzzybear/coverage/Coverage.py
+++ fuzzybear/coverage/Coverage.py
@@ -1,5 +1,6 @@
 from pwn import *
 from capstone import *
+from pprint import pprint
 
 # ================================================================= #
 
@@ -15,84 +16,99 @@ def update_coverage(address):
 	print(f"[>>] Updating coverage, hit {hex(address)}")
 	print("[>>] Not implemented")
 
-'''Alternative data structure I talked about on Discord
+'''Ideas:
+	- Once you have code blocks working properly
+	- You should be able to detect if you some sort of cyclic behaviour	
+	- 
+'''
+# Assume that the base addr of the binary is passed in so it 
+# can resolve function names.
+class FunctionCalls:
+	def __init__(self, binaryBase, pid, elf=None):
+		self.calls = {}
+		self.binaryBase = binaryBase
+		self.pid = pid
+		self.elf = elf # pwntools.elf.ELF
+
+	# Think it's better to use funcAddr as the key on the offchance
+	# that the binary is stripped of symbols.
+	# A potentially major problem here is that functions that are called multiple times,
+	# will only have 1 entry in the dictionary since it's indexed by function address. 
+	def add(self, functionCall):
+		self.calls[str(functionCall.funcAddr)] = functionCall
+
+
+	def resolveFunctionNames(self):
+		addressNameMap = {}
+		# elf.functions returns a dictionary that maps function names to function objects.
+		# function object basically encapsulate some data about a function, incl its addr.
+		for fnName, fnObj in self.elf.functions.items():
+			addressNameMap[str(hex(fnObj.address))] = fnName
+		pprint(addressNameMap)
+
+		for key, val in self.calls.items():
+			try:
+				self.calls[key].setFunctionName(addressNameMap[str(key)])
+			except:
+				continue
+
+	def __str__(self):
+		string = ""
+		for key, val in self.calls.items():
+			string += f'{val.instAddr} = call {val.funcAddr} / {val.funcName}\n'
+		return string
+
+# Stored all values as strings since funcAddr will be used as a key
+# so for consistency they're all strings.
+class FunctionCall:
+	def __init__(self, instAddr, funcAddr):
+		self.instAddr = instAddr
+		self.funcAddr = funcAddr
+		self.funcName = None
+		self.timesCalled  = 0
+
+	def setFunctionName(self, name):
+		self.funcName = name
+
+	def called(self):
+		self.timesCalled += 1
+
+	def nCalled(self):
+		return self.timesCalled
+
+
 class JumpBlocks:
 	def __init__(self):
-		self.jumps = {}
+		self.blocks = {}
 		self.watching = False
 
 	def watch(self):
 		self.watching = True
 
 	def add(self, jumpBlock):
-		self.jumpBlocks[str(jumpBlock.start)] = jumpBlock
+		self.blocks[str(hex(jumpBlock.start))] = jumpBlock
 
 	# Assuming addr is a pointer thats jumped to in the binary.
 	def isNewPath(self, addr):
 		return self.jumpBlocks[str(addr)] == 0
 
+	def __str__(self):
+		string = ""
+		for key, val in self.blocks.items():
+			string += f'{hex(val.start)} - {hex(val.end)}\n'
+		return string
+
 class JumpBlock:
 	def __init__(self, start, end):
 		self.start = start
 		self.end = end
-		self.nReached = 0
-	
-	def reached(self):
-		self.nReached += 1
-	
-	def getReached(self):
-		return self.nReached
-'''
-
-# A binary tree where the root is main, and each child is appended when a j**
-# instruction is seen in the binary.
-
-# Feel free to change whatever the fuck you want.
-class JumpTree:
-	# For the root node start will be the address of main(). For all subsequent nodes:
-	# The start address will be wherever some previous code block jumped too. end will be
-	# wherever the address that the end of this block jumps to.
-	# Each jump will create a new node in the tree
-	
-	def __init__(self, start, end, mnemonic):
-		# Hardcoded address of main(). Will need to be more robust than this more likely
-		self.start = start
-		self.end = end
-		self.mnemonic = mnemonic
-		self.right = None
-		self.left = None
-		self.numberTimesReached = 0
-
-	# I memed 2521, this will probably yeet to infinity. Also not sure how helpful a tree
-	# structured like this will be for just basic jump blocks
-	def add(self, tree):
-		if tree.start > self.start:
-			if self.right is None:
-				self.right = tree
-			else:
-				self.right.add(tree)
-		else:
-			if self.left is None:
-				self.left = tree
-			else:
-				self.left.add(tree)
+		self.timesReached = 0
 
 	def reached(self):
-		self.numberTimesReached += 1
-	
+		self.timesReached += 1
+
 	def nReached(self):
-		return self.numberTimesReached
-
-	def show(self, tree):
-		if tree is None:
-			return
-		#print(f'{hex(tree.start)} - {hex(tree.end)}')
-
-		# mnemonic's arent being stored correctly
-		print(f'{hex(tree.start)} - {hex(tree.end)}::{self.mnemonic}') 
-
-		self.show(tree.left)
-		self.show(tree.right)
+		return self.timesReached
 
 # TODO :: Finish implementing
 class Coverage:
@@ -112,28 +128,50 @@ class Coverage:
 		opcodes = elf.section('.text')
 
 		# No pie, so binary base should always be the same
-		startOfInstructions = 0x80490f0
+		#startOfInstructions = 0x80490f0
 		addrMain = elf.symbols['main']
-		print(hex(addrMain))
-
+		addrStart = elf.symbols['_start']
+		#print(hex(addrMain))
 		current_block = 0
 		last_block = 0
 		i = 0
+		j = 0
 		startOfNextBlock = 0
 		# We start storing from after main() since I don't think jumps before main are
 		# relevant for code coverage? Although maybe we need to include GOT stuffs ?
-		for op in target.disasm(opcodes, startOfInstructions):
+		for op in target.disasm(opcodes, addrStart):
 			# This finds all jump instructions in the binary. Seems to be a useful marker
-			if 'j' in op.mnemonic and op.address > addrMain:
-				print("0x%x:\t%s\t%s" %(op.address, op.mnemonic, op.op_str))
+			if 'j' in op.mnemonic and op.address >= addrMain and 'eax' not in op.op_str:
+				#print("0x%x:\t%s\t%s" %(op.address, op.mnemonic, op.op_str))
 				if i == 0:
-					jumpTree = JumpTree(addrMain, op.address, op.mnemonic)					
+					jumpBlocks = JumpBlocks()
+					jumpBlocks.add(JumpBlock(addrMain, op.address))
+					#jumpTree = JumpTree(addrMain, op.address, op.mnemonic)					
 					startOfNextBlock = int(op.op_str,0x10) 
 				else:
-					jumpTree.add(JumpTree(startOfNextBlock,  op.address ,op.mnemonic))
+					#jumpTree.add(JumpTree(startOfNextBlock,  op.address ,op.mnemonic))
+					jumpBlocks.add(JumpBlock(startOfNextBlock, op.address))
 					startOfNextBlock = int(op.op_str,0x10) 
 				i += 1
-		jumpTree.show(jumpTree)
+			if 'call' in op.mnemonic:
+				print("0x%x:\t%s\t%s" %(op.address, op.mnemonic, op.op_str))
+				if j == 0:
+					#Will get binaryBase from /proc/{pid}/maps
+					functionCalls = FunctionCalls(0x8048000, 1, elf)
+				try:
+					functionCalls.add(FunctionCall(hex(op.address), hex(int(op.op_str, 0x10))))
+					# print('added function call')
+				except:
+					# if you don't call an addr, then it won't be saved in functionCalls. For example `call eax`
+					# won't be stored as a function call. This can be implemented but not sure how helpful
+					# it'll be.
+					pass
+				j += 1
+
+		functionCalls.resolveFunctionNames()
+		print(functionCalls)
+		print(jumpBlocks)
+		#jumpTree.show(jumpTree)
 			#if len(op.groups) > 0:
 				#print(f"Block{'':5}{op.groups}\n")
 				
--- fuzzybear/coverage/testCoverage.py
+++ fuzzybear/coverage/testCoverage.py
@@ -2,7 +2,8 @@ from Coverage import Coverage
 from pwn import *
 
 # Make sure this path is right for you.
-elf = ELF('../../../binaries/plaintext2')
+elf = ELF('./test')
+#'/home/lucas/Comp/UNSW/2021/term2/6447/week3/wargame/bin/formatrix
 
 c = Coverage(elf)
 c.gen_code_paths()
