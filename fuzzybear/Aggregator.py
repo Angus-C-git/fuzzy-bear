ee529f9ab4c10153abe044b7d718af183fef0b2f
Angus-C-git <anguscornall@gmail.com>
2021-08-08 17:45:47 +1000

    UPDATE :: agregator

--- fuzzybear/Aggregator.py
+++ fuzzybear/Aggregator.py
@@ -29,9 +29,9 @@ def write_crash(crashing_input):
 class Aggregator():
 	def __init__(self, binary, input_file):
 		print(f"   [>>] Running fuzzer against <{binary.split('/')[-1]}> mutating <{input_file.split('/')[-1]}>")
-		self.harness = Harness(binary)
 		self.codec = codec.detect(input_file)
-		print(f'   [DEBUG] {self.codec}')
+		self.harness = Harness(binary, self.codec)
+		# print(f'   [DEBUG] {self.codec}')
 		self.base_file = input_file
 
 	# TODO :: Should internals be exported
@@ -51,13 +51,12 @@ class Aggregator():
 		## TMP RUNNER >> ##
 		while True:
 			inputs = Generator.run()
-
 		
 			for input in inputs:
 				# print(f"	 [DEBUG] mutation was {input}")
 				response_code = self.harness.open_pipe(input)
 				# print(f"\n   [DEBUG] Aggregator received {response_codes.lookup(response_code)} from binary")
-				if (response_code): 
+				if (response_code == -11): 
 					write_crash(input)
 					exit(0)				# exit on crash ? 
 
